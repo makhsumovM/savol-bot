8112fab00950e188c9cf36acfc0465ea573ef291
Abdulloh <sultonzoda2011@gmail.com>
2025-12-16 14:34:54 +0500

    feat: refine ModeToggle component styles and improve padding

--- src/ui/common/modeToggle/modeToggle.tsx
+++ src/ui/common/modeToggle/modeToggle.tsx
@@ -10,12 +10,19 @@ export function ModeToggle() {
   const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
 
   return (
-    <ToggleGroup type="single" className="flex gap-2 bg-card/50 backdrop-blur rounded-lg p-1 border border-border">
+    <ToggleGroup
+      type="single"
+      className="flex  gap-2 bg-card/50 backdrop-blur rounded-lg p-[2px] border border-border"
+    >
       <ToggleGroupItem
         value="light"
         onClick={toggleTheme}
-        className={`flex items-center justify-center p-2 rounded-md cursor-pointer transition-all duration-300
-          ${theme === 'light' ? 'bg-primary/20 text-primary' : 'text-muted-foreground hover:bg-primary/10'}`}
+        className={`flex items-center justify-center p-1.5 rounded-md cursor-pointer transition-all duration-300
+          ${
+            theme === 'light'
+              ? 'bg-primary/20 text-primary'
+              : 'text-muted-foreground hover:bg-primary/10'
+          }`}
         aria-label="Light mode"
       >
         <Sun className="h-5 w-5" />
@@ -24,8 +31,12 @@ export function ModeToggle() {
       <ToggleGroupItem
         value="dark"
         onClick={toggleTheme}
-        className={`flex items-center justify-center p-2 rounded-md cursor-pointer transition-all duration-300
-          ${theme === 'dark' ? 'bg-primary/20 text-primary' : 'text-muted-foreground hover:bg-primary/10'}`}
+        className={`flex items-center justify-center p-1.5 rounded-md cursor-pointer transition-all duration-300
+          ${
+            theme === 'dark'
+              ? 'bg-primary/20 text-primary'
+              : 'text-muted-foreground hover:bg-primary/10'
+          }`}
         aria-label="Dark mode"
       >
         <Moon className="h-5 w-5" />
