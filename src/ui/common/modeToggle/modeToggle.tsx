456131efb0fa16ddf2708b76e2211f994d30d0ad
Abdulloh <sultonzoda2011@gmail.com>
2025-12-16 19:57:15 +0500

    feat: refactor ModeToggle component for improved theme handling and code clarity

--- src/ui/common/modeToggle/modeToggle.tsx
+++ src/ui/common/modeToggle/modeToggle.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
@@ -7,21 +7,25 @@ import { ToggleGroup, ToggleGroupItem } from '@/ui/toggle-group/toggle-group'
 export function ModeToggle() {
   const { theme, setTheme } = useTheme()
 
-  const toggleTheme = (mode: "light" | "dark") => setTheme(mode)
+  const currentTheme = theme || 'dark'
+  const toggleTheme = (mode: 'light' | 'dark') => setTheme(mode)
 
   return (
     <ToggleGroup
       type="single"
+      value={currentTheme}
       className="flex gap-1 bg-card/80 backdrop-blur-xl rounded-xl p-[4px] border border-border/50 shadow-sm"
     >
       <ToggleGroupItem
         value="light"
-        onClick={() => toggleTheme("light")}
+        onClick={() => toggleTheme('light')}
         className={`
-          flex items-center justify-center  rounded-lg cursor-pointer transition-all duration-300
-          ${theme === 'light'
-            ? 'bg-linear-to-br from-yellow-300/30 to-yellow-500/30 text-yellow-600 shadow-md'
-            : 'text-muted-foreground hover:bg-yellow-200/10'}
+          flex items-center justify-center rounded-lg cursor-pointer transition-all duration-300
+          ${
+            currentTheme === 'light'
+              ? 'bg-linear-to-br from-yellow-300/30 to-yellow-500/30 text-yellow-600 shadow-md'
+              : 'text-muted-foreground hover:bg-yellow-200/10'
+          }
         `}
         aria-label="Light mode"
       >
@@ -30,12 +34,14 @@ export function ModeToggle() {
 
       <ToggleGroupItem
         value="dark"
-        onClick={() => toggleTheme("dark")}
+        onClick={() => toggleTheme('dark')}
         className={`
-          flex items-center justify-center  rounded-lg cursor-pointer transition-all duration-300
-          ${theme === 'dark'
-            ? 'bg-linear-to-br from-purple-600/30 to-purple-900/30 text-purple-500 shadow-md'
-            : 'text-muted-foreground hover:bg-purple-200/10'}
+          flex items-center justify-center rounded-lg cursor-pointer transition-all duration-300
+          ${
+            currentTheme === 'dark'
+              ? 'bg-linear-to-br from-purple-600/30 to-purple-900/30 text-purple-500 shadow-md'
+              : 'text-muted-foreground hover:bg-purple-200/10'
+          }
         `}
         aria-label="Dark mode"
       >
