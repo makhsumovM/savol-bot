a05ba102cbff8416972f3de126edec05f5653180
KosimovM <yqosimov7@gmail.com>
2025-12-17 19:09:24 +0500

    ref: инро низ барои заьъбонро тагирдодан

--- src/api/randomAPi.ts
+++ src/api/randomAPi.ts
@@ -1,10 +1,16 @@
-import {RandomQuestion} from '@/types/random'
+import { RandomQuestion } from '@/types/random'
 import axios from 'axios'
 
-export const Randonapi = async (): Promise<RandomQuestion[]> => {
+export const Randonapi = async (
+  lang: string
+): Promise<RandomQuestion[]> => {
   try {
-    const response = await axios.get('/api/gemini/random')
+    const response = await axios.get(
+      `/api/gemini/random?lang=${encodeURIComponent(lang)}`
+    )
+
     const questions: RandomQuestion[] = JSON.parse(response.data.result)
+
     return questions
   } catch (error) {
     console.error(error)
