597968982d5f0a83bccd02744ff938ea2684e436
KosimovM <yqosimov7@gmail.com>
2025-12-17 19:08:41 +0500

    ref: инро барои забонро ивазкардан созкардам

--- src/api/marathonApi.ts
+++ src/api/marathonApi.ts
@@ -1,10 +1,13 @@
 import { MarathonQuestion } from '@/types/marathon'
 import axios from 'axios'
 
-export const marathonApi = async (lang: string): Promise<MarathonQuestion[]> => {
+export const marathonApi = async (lang: string):
+ Promise<MarathonQuestion[]> => {
   try {
-    const response = await axios.get(`/api/gemini/marathon?lang=${encodeURIComponent(lang)}`)
-    const questions: MarathonQuestion[] = JSON.parse(response.data.result)
+    const response = await axios.get
+    (`/api/gemini/marathon?lang=${encodeURIComponent(lang)}`)
+    const questions: MarathonQuestion[] = JSON.parse(response.
+      data.result)
     return questions
   } catch (error) {
     console.error(error)
