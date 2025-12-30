aeffdf21679c7f7bd508fce37ecde6803e0e92b1
Abdulloh <sultonzoda2011@gmail.com>
2025-12-30 12:09:24 +0500

    feat: implement proxy middleware for authentication handling

---
+++ proxy.ts
@@ -0,0 +1,34 @@
+import { CookieKey } from '@/lib/utils/cookies'
+import { NextRequest, NextResponse } from 'next/server'
+
+export function proxy(request: NextRequest) {
+  const token = request.cookies.get(CookieKey.token)?.value
+  const { pathname } = request.nextUrl
+
+  if (token) {
+    if (pathname === '/login' || pathname === '/register') {
+      const url = request.nextUrl.clone()
+      url.pathname = '/'
+      return NextResponse.redirect(url)
+    }
+    return NextResponse.next()
+  }
+
+  if (pathname === '/login' || (pathname === '/register' && !token)) {
+    return NextResponse.next()
+  }
+
+  if (pathname.startsWith('/my-best') && !token) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
+  }
+
+  const url = request.nextUrl.clone()
+  url.pathname = '/login'
+  return NextResponse.redirect(url)
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+}
