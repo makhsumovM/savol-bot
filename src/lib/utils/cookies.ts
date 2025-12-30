86598493dc086f36027798492e8402c032b4b0ad
Abdulloh <sultonzoda2011@gmail.com>
2025-12-30 12:12:30 +0500

    feat: generalize cookie helpers to support arbitrary keys

--- src/lib/utils/cookies.ts
+++ src/lib/utils/cookies.ts
@@ -2,13 +2,13 @@ import Cookies from 'js-cookie'
 export enum CookieKey {
   token = 'token',
 }
-export function getToken() {
-  return Cookies.get(CookieKey.token)
+export function getCookie(key: string) {
+  return Cookies.get(key || CookieKey.token)
 }
 
-export function setToken(token: string) {
-  return Cookies.set(CookieKey.token, token)
+export function setCookie({ token, key }: { token: string; key?: string }) {
+  return Cookies.set(key || CookieKey.token, token)
 }
-export function removeToken() {
-  return Cookies.remove(CookieKey.token)
+export function removeCookie(key: string) {
+  return Cookies.remove(key || CookieKey.token)
 }
