942e9e14ea40ab9dd2c4f752a29b389c215d44e7
Abdulloh <sultonzoda2011@gmail.com>
2026-01-16 06:32:47 +0500

    feat: add codeLanguage to RandomQuestion type

--- src/types/random.ts
+++ src/types/random.ts
@@ -1,8 +1,7 @@
 export interface RandomQuestion {
   question: string
   code: string | null
-  answers: string[]
-  correctIndex: number
-  difficulty: 'easy' | 'medium' | 'hard' | 'very-hard' | 'expert'
-   type: 'frontend' | 'backend'
+  codeLanguage: 'javascript' | 'typescript' | 'html' | 'css' | 'tsx' | 'csharp' | null
+  answers: [string, string, string, string]
+  correctIndex: 0 | 1 | 2 | 3
 }
