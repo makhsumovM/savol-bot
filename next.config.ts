ac757414b5947bd006d1f40b08af9783e708a96f
Abdulloh <sultonzoda2011@gmail.com>
2026-01-21 07:01:40 +0500

    ref:url file api

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  reactCompiler: true, 
+  reactCompiler: true,
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**",
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
