ff2561f2427289fa11762d99c9a160540ec8559e
Your <you@example.com>
2026-01-22 18:50:37 +0500

    Fix: add randomApi and correct import alias

--- src/app/random/randomClient.tsx
+++ src/app/random/randomClient.tsx
@@ -10,7 +10,7 @@ import { RandomQuestion } from '@/types/random'
 import Image from 'next/image'
 import react from '../../../public/react.png'
 import QuestionCard from '@/ui/common/questionCards/random/questionCard'
-import { randomApi } from '@/api/randomApi'
+import { randomApi } from '@/app/random/randomApi'
 
 
 const QUESTIONS_PER_BLOCK = 10
