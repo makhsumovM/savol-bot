5681635a8b00760885f28a54e72f7282c9bc563c
Abdulloh <sultonzoda2011@gmail.com>
2026-02-12 05:39:03 +0500

    feat: add mobdevScore and achievement date to IMyBest interface

--- src/types/my-best.ts
+++ src/types/my-best.ts
@@ -1,8 +1,10 @@
 export interface IMyBest {
   bestFrontendScore: number
   bestBackendScore: number
+  bestMobdevScore: number
   frontendAchievedAt: string
   backendAchievedAt: string
+  mobdevAchievedAt: string
 }
 
 export interface IMyBestResponse {
