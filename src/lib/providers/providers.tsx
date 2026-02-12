fe7f9d8c0c04e1630d7ed5308f273895cfcbe0ee
Abdulloh <sultonzoda2011@gmail.com>
2026-02-12 05:37:10 +0500

    ref: remove unnecessary token cleanup effect from Providers

--- src/lib/providers/providers.tsx
+++ src/lib/providers/providers.tsx
@@ -2,21 +2,16 @@
 
 import { QueryClient } from '@tanstack/query-core'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactNode, useEffect } from 'react'
+import { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import { store } from '@/lib/store/store'
 import { ThemeProvider } from '@/lib/providers/theme-provider'
 import { Toaster } from 'sonner'
-import { setupTokenCleanup } from '@/lib/utils/jwt'
 import AOSProvider from '@/lib/providers/aos-provider'
 
 const queryClient = new QueryClient()
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  useEffect(() => {
-    setupTokenCleanup()
-  }, [])
-
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
