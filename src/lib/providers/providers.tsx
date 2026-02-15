ee225edb1cb0f8123567ea64a5ca7e855f5c6fd9
Abdulloh <sultonzoda2011@gmail.com>
2026-02-15 05:20:56 +0500

    chore: remove unused Redux Provider from root Providers wrapper

--- src/lib/providers/providers.tsx
+++ src/lib/providers/providers.tsx
@@ -3,8 +3,6 @@
 import { QueryClient } from '@tanstack/query-core'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
-import { Provider } from 'react-redux'
-import { store } from '@/lib/store/store'
 import { ThemeProvider } from '@/lib/providers/theme-provider'
 import { Toaster } from 'sonner'
 import AOSProvider from '@/lib/providers/aos-provider'
@@ -13,14 +11,12 @@ const queryClient = new QueryClient()
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider enableSystem attribute="class" defaultTheme="dark" disableTransitionOnChange>
-          <Toaster closeButton richColors />
-          <AOSProvider>{children}</AOSProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider enableSystem attribute="class" defaultTheme="dark" disableTransitionOnChange>
+        <Toaster closeButton richColors />
+        <AOSProvider>{children}</AOSProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
