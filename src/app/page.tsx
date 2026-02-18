a2d286c5ee44c4b3a84211ab02640b17d72cbc43
Abdulloh <sultonzoda2011@gmail.com>
2026-02-18 20:55:59 +0500

    ref: replace ModeCards with ModeSection in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,41 +2,17 @@
 
 import { useQuery } from '@tanstack/react-query'
 import { getTotalUsers } from '@/api/userApi'
-import { useTranslation } from 'react-i18next'
 import { HeroSection } from '@/ui/home/heroSection'
 import { FeaturesSection } from '@/ui/home/featuresSection'
-import { ModeCards } from '@/ui/home/modeCards'
 import { FinalCtaSection } from '@/ui/home/finalCtaSection'
+import ModeSection from '@/ui/home/modeSection'
 
 export default function Home() {
-  const { t } = useTranslation()
-
   const { data: totalUsers } = useQuery({
     queryKey: ['totalUsers'],
     queryFn: getTotalUsers,
     staleTime: 1000 * 60 * 5,
   })
-
-  const modes = [
-    {
-      id: 'marathon',
-      title: t('modes.marathon.title'),
-      badge: t('modes.marathon.badge'),
-      desc: t('modes.marathon.description'),
-    },
-    {
-      id: 'random',
-      title: t('modes.classic.title'),
-      badge: t('modes.classic.badge'),
-      desc: t('modes.classic.description'),
-    },
-    {
-      id: 'coding',
-      title: t('modes.coding.title'),
-      badge: t('modes.coding.badge'),
-      desc: t('modes.coding.description'),
-    },
-  ]
 
   return (
     <div className="relative min-h-screen overflow-hidden">
@@ -51,9 +27,7 @@ export default function Home() {
       <div className="relative">
         <HeroSection totalUsers={totalUsers?.totalUsers} />
         <FeaturesSection />
-        <section className="relative pb-20">
-          <ModeCards modes={modes} />
-        </section>
+        <ModeSection />
         <FinalCtaSection totalUsers={totalUsers?.totalUsers} />
       </div>
     </div>
