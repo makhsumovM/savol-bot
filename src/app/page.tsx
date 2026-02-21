b90e05eba7d435b33e24042cfc1b37ba524b21aa
Abdulloh <sultonzoda2011@gmail.com>
2026-02-21 12:35:29 +0500

    feat: Add initial home page with Hero, Mode, and Final CTA sections.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,14 @@
-'use client'
-
-import { useQuery } from '@tanstack/react-query'
-import { getTotalOnlineUser } from '@/api/userApi'
 import { HeroSection } from '@/ui/home/heroSection'
 import { FinalCtaSection } from '@/ui/home/finalCtaSection'
 import ModeSection from '@/ui/home/modeSection'
 
 export default function Home() {
-  const { data: totalUsers, isPending } = useQuery({
-    queryKey: ['totalUsers'],
-    queryFn: getTotalOnlineUser,
-  })
-
   return (
     <div className="relative min-h-screen overflow-hidden selection:bg-primary/20 selection:text-primary">
-      { }
-
-
       <main className="relative z-10">
-        <HeroSection totalUsers={totalUsers?.totalUsers} isPending={isPending} />
+        <HeroSection />
         <ModeSection />
-        <FinalCtaSection totalUsers={totalUsers?.totalUsers} />
+        <FinalCtaSection />
       </main>
     </div>
   )
